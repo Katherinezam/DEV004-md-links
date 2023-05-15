14ec669b54947f79605b8dd940df1c84bd2551f7
Katherinezam <rkafaella@gmail.com>
2023-05-15 16:11:42 -0500

    funcion leer archivo

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 //  fs es un módulo de Node proporciona funciones asíncronas y síncronas para trabajar con el sistema de archivos. 
 
+import { promises } from 'dns';
 import fs from 'fs'
 import path from 'path'
 
@@ -17,8 +18,18 @@ const esArchivoMd = (ruta) => path.extname(ruta) === '.md'
 
 // Función que lee un archivo Markdown y devuelve su contenido
 // UTF-8 es un estándar de codificación de caracteres
-const leerArchivoMd = (ruta) => fs.readFileSync(ruta, 'utf8');
+const leerArchivoMd = (ruta) => fs.readFile(ruta, 'utf8');
 
+const leerArchivo = (ruta) => new Promise(function (resolve,reject ){
+    fs.readFile(ruta, 'utf8', function (err, data)  {
+        if( err){
+            reject(err)
+        } else {
+            resolve(data)
+        }
+
+    });
+})
 
 
 
