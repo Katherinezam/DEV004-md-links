5f180df2f4d5dc38aeb694e2b84952d45fe91b84
Katherinezam <rkafaella@gmail.com>
2023-05-15 19:19:25 -0500

    Pruebas

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,49 +1,59 @@
 import { mdLinks } from "../mdlinks.js";
 import { existeRuta } from "../api.js";
+import { esRutaAbsoluta } from "../api.js";
 
-describe('mdLinks', () => {
-
-it('mdlink procesa un solo archivo con tres links sin validar', () => {
-  const ruta = 'ejemplo.md';
-  // llamo la funcion mdLinks con parametros de prueba const ruta = ejemplo.md
-  // mi funcion debe retornar una promesa es por eso que la validacion osea el expectdebe estar dentro del then
-  // debo retornar la promesa ya que es un requisito de jest para probar codigo asincrono
-  return mdLinks (ruta, {validate: false})
-  .then(
-    // el arreglo es un parametro de la funcion flecha del then
-    (array) => {
-      expect(array).toEqual([
-        {
-          href:'https://es.wikipedia.org/wiki/Markdown',
-          text: 'Markdown',
-          file: 'ejemplo.md',
-        },
-        {
-          href:'https://nodejs.org/es/',
-          text: 'Node.js',
-          file: 'ejemplo.md',
-        },
-        {
-          href:'https://developers.google.com/v8/',
-          text: 'motor de JavaScript V8 de Chrome',
-          file: 'ejemplo.md',
-        }
-      ]);
-    }
-  )
+describe("mdLinks", () => {
+  it("mdlink procesa un solo archivo con tres links sin validar", () => {
+    const ruta = "ejemplo.md";
+    // llamo la funcion mdLinks con parametros de prueba const ruta = ejemplo.md
+    // mi funcion debe retornar una promesa es por eso que la validacion osea el expectdebe estar dentro del then
+    // debo retornar la promesa ya que es un requisito de jest para probar codigo asincrono
+    return mdLinks(ruta, { validate: false }).then(
+      // el arreglo es un parametro de la funcion flecha del then
+      (array) => {
+        expect(array).toEqual([
+          {
+            href: "https://es.wikipedia.org/wiki/Markdown",
+            text: "Markdown",
+            file: "ejemplo.md",
+          },
+          {
+            href: "https://nodejs.org/es/",
+            text: "Node.js",
+            file: "ejemplo.md",
+          },
+          {
+            href: "https://developers.google.com/v8/",
+            text: "motor de JavaScript V8 de Chrome",
+            file: "ejemplo.md",
+          },
+        ]);
+      }
+    );
 
     // console.log('FIX ME!');
   });
-
 });
 
-//funciona sincrona
-describe('existeRuta', () => {
-it('Deberia recibir true si existe la ruta', () => {
-expect(existeRuta('ejemplo.md')).toBe(true)
+//funcion sincrona
+describe("existeRuta", () => {
+  it("debería devolver true si la ruta existe", () => {
+    expect(existeRuta("ejemplo.md")).toBe(true);
+  });
+
+  it("debería devolver false si la ruta NO existe", () => {
+    expect(existeRuta("noexiste.md")).toBe(false);
+  });
 });
 
-it('Deberia recibir false si NO existe la ruta', () => {
-  expect(existeRuta('noexiste.md')).toBe(false)
-});
+describe("esRutaAbsoluta", () => {
+  it("debería devolver true si la ruta es absoluta", () => {
+    expect(esRutaAbsoluta("/ruta/absoluta")).toBe(true);
+    // expect(esRutaAbsoluta('C:\\ruta\\absoluta')).toBe(true);
+  });
+
+  it("debería devolver false si la ruta NO es absoluta", () => {
+    expect(esRutaAbsoluta("ruta/relativa")).toBe(false);
+    // expect(esRutaAbsoluta('./archivo.md')).toBe(false);
+  });
 });
